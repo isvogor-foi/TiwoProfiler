76f9aee19d8162dcb01cad177b486edf1a456a87
isvogor <isvogor@foi.hr>
2015-08-12 18:42:31 +0200

    Updated with MissionManager profiler (It is supposed to be used in conjunction with VisualVM)

--- TiwoProfiler/src/com/tiwo/mm/MissionManagerProfilerMain.java
+++ TiwoProfiler/src/com/tiwo/mm/MissionManagerProfilerMain.java
@@ -1,9 +1,36 @@
 package com.tiwo.mm;
 
-public class MissionManagerProfilerMain {
+import java.util.Scanner;
 
+import com.tiwo.mission.MissionManagerMain;
+
+public class MissionManagerProfilerMain {
+	/**
+	 * Profiler project for the MissionManager project / component
+	 * The main method / interface is the constructor, wich automatically loads the mission (*.mis) file
+	 * and executes it
+	 * 
+	 * 
+	 */
+	 
 	public static void main(String[] args) {
 		
+		pauseProg();
+		for(int i = 0; i < 2000; i++)
+			profileMM();
+		
 	}
+	
+	public static void profileMM(){
+		for(int i = 1; i <= 5; i++){
+			MissionManagerMain mm = new MissionManagerMain("/home/ivan/git/TiwoProfiler/TiwoProfiler/src/com/tiwo/mm/demomissions/" + i + ".mis");
+		}
+	}
+	
+	public static void pauseProg(){
+		System.out.println("Press enter to continue...");
+		Scanner keyboard = new Scanner(System.in);
+		keyboard.nextLine();
+		}
 
 }
